e50b094d3c4f9c78a390d8379e575987bee145c9
Trenton <050trenton@gmail.com>
2022-04-29 17:40:34 -0500

    added the popup pin

--- script.js
+++ script.js
@@ -21,16 +21,19 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
+      //   console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(map).bindPopup('workout').openPopup();
+      });
     },
     function () {
       alert('Could not get current position');
