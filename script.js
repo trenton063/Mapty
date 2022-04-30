f3fc717a4239c922567a6a86affe02ef540f9bce
Trenton <050trenton@gmail.com>
2022-04-29 19:17:28 -0500

    added teh cycling toggle classlist

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ if (navigator.geolocation) {
 form.addEventListener('submit', function (e) {
     e.preventDefault();
   // Display marker
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
   console.log(mapEvent);
   const { lat, lng } = mapEvent.latlng;
-
   L.marker([lat, lng])
     .addTo(map)
     .bindPopup(
@@ -64,3 +64,8 @@ form.addEventListener('submit', function (e) {
     .setPopupContent('Workout')
     .openPopup();
 });
+
+inputType.addEventListener('change', function() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
